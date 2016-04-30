186860430de091eb5c8084611553adb118d638bd
` <dev@devdemac-mini.local>
2016-04-30 21:46:16 +0800

    update

---
+++ iVegetables/Controller/GuideViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GuideViewController.swift
+//  iVegetables
+//
+//  Created by dev on 16/4/30.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GuideViewController: BaseViewController {
+    
+}
---
+++ iVegetables/Library/Base/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  iVegetables
+//
+//  Created by dev on 16/4/30.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+}
