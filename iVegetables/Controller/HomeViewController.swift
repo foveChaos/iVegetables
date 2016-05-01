b2ab4976f3f412607416a1b679560a7553def009
` <dev@devdemac-mini.local>
2016-05-01 11:08:30 +0800

    update

---
+++ iVegetables/Controller/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  iVegetables
+//
+//  Created by dev on 16/5/1.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: BaseViewController {
+    
+    @IBOutlet private weak var guideContainer: UIView!
+}
