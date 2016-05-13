455d9c61add3ba712b4a636bb5acbc4683f39bd3
realsys <foveChaos@yeah.net>
2016-05-13 17:52:09 +0800

    update

--- iVegetables/AppDelegate.swift
+++ iVegetables/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let locationManager = CLLocationManager()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
         return true
     }
 
